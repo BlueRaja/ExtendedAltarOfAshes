c3096db8dc1ce67dac31f30f6712b5c736e11bcd
ViggyJ777 <fish77777@gmail.com>
2025-11-12 13:53:04 -0800

    Update TraitData.lua
    fixed if/then syntax error

--- src/TraitData.lua
+++ src/TraitData.lua
@@ -1,5 +1,5 @@
 if not ExtendedAltarOfAshes.Config.Enabled then return end
-if config.LiteMode.Enabled then return end
+if config.LiteMode then return end
 
 ModUtil.LoadOnce(function ()
     TraitRarityData.RarityUpgradeOrder = { "Common", "Rare", "Epic", "Heroic", "Legendary", }
