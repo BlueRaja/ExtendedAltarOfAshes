dad3e9a507d1de75ce6b09a9718c0081d9a40940
ViggyJ777 <fish77777@gmail.com>
2025-11-12 20:08:07 -0800

    Update config.lua

--- src/config.lua
+++ src/config.lua
@@ -9,5 +9,5 @@ local description = {
         Enabled = "Enable to activate Lite Mode: 40 max grasp, maximum heroic arcana (not legendary), and better mod compatibility"
     },
 }
-ExtendedAltarOfAshes.Config = config
+-- ExtendedAltarOfAshes.Config = config (not needed anymore?)
 return config
