84381ca10db63681a4304801f7a786d36ec648b8
BlueRaja <BlueRaja.admin@gmail.com>
2026-04-26 13:02:50 -0700

    BUGFIX: Save/reload issue caused by Post Launch Patch 2

--- src/MetaUpgradeData.lua
+++ src/MetaUpgradeData.lua
@@ -1,30 +1,38 @@
 if not ExtendedAltarOfAshes.Config.Enabled then return end
 
-ModUtil.LoadOnce(function ()
-    local defaultNumUpgades = #MetaUpgradeCostData.MetaUpgradeLevelData
-	local graspToAdd = ExtendedAltarOfAshes.Config.MaxGrasp - defaultNumUpgades
-    for i = 1, graspToAdd do
-		-- Cost will be X Psyche, 1 Star Dust, 1 Shadow
-        table.insert(MetaUpgradeCostData.MetaUpgradeLevelData, { CostIncrease = 1, ResourceCost = { MemPointsCommon = 500+40*i, Mixer5Common = 1, MixerShadow = 1 }})
-        table.insert(ScreenData.MetaUpgradeCardLayout.BonePositions, ScreenData.MetaUpgradeCardLayout.BonePositions[defaultNumUpgades]) -- Fix crash due to missing "bone position" data
-    end
+local function multiplyCosts(costTable, multiplier)
+	local result = {}
+	for key, value in pairs(costTable) do
+		result[key] = math.ceil(value * multiplier)
+	end
+	return result
+end
 
-	local function multiplyCosts(costTable, multiplier)
-		local result = {}
-		for key, value in pairs(costTable) do
-			result[key] = math.ceil(value * multiplier)
-		end
-		return result
+-- Adds heroic/legendary upgrade levels to arcana tables
+local function addAdditionalUpgrades(upgradeTable, skipLegendary)
+	if upgradeTable == nil or upgradeTable.UpgradeResourceCost == nil or upgradeTable._ExtendedAltarLevelsApplied then
+		return
 	end
 
-	-- Adds heroic/legendary upgrade levels to arcana tables
-	local function addAdditionalUpgrades(upgradeTable, skipLegendary)
-		local lastUpgrade = upgradeTable.UpgradeResourceCost[#upgradeTable.UpgradeResourceCost]
-		if lastUpgrade then
-			table.insert(upgradeTable.UpgradeResourceCost, multiplyCosts(lastUpgrade, 1.5))
-			if ExtendedAltarOfAshes.Config.AddLegendaryArcana and not skipLegendary then
-				table.insert(upgradeTable.UpgradeResourceCost, multiplyCosts(lastUpgrade, 2))
-			end
+	local lastUpgrade = upgradeTable.UpgradeResourceCost[#upgradeTable.UpgradeResourceCost]
+	if lastUpgrade then
+		table.insert(upgradeTable.UpgradeResourceCost, multiplyCosts(lastUpgrade, 1.5))
+		if ExtendedAltarOfAshes.Config.AddLegendaryArcana and not skipLegendary then
+			table.insert(upgradeTable.UpgradeResourceCost, multiplyCosts(lastUpgrade, 2))
+		end
+	end
+
+	upgradeTable._ExtendedAltarLevelsApplied = true
+end
+
+local function applyExtendedMetaUpgradeData()
+	local currentMaxGrasp = #MetaUpgradeCostData.MetaUpgradeLevelData
+	if currentMaxGrasp < ExtendedAltarOfAshes.Config.MaxGrasp then
+		local templateBonePosition = ScreenData.MetaUpgradeCardLayout.BonePositions[currentMaxGrasp]
+		for i = currentMaxGrasp + 1, ExtendedAltarOfAshes.Config.MaxGrasp do
+			-- Cost will be X Psyche, 1 Star Dust, 1 Shadow
+			table.insert(MetaUpgradeCostData.MetaUpgradeLevelData, { CostIncrease = 1, ResourceCost = { MemPointsCommon = 500 + 40 * i, Mixer5Common = 1, MixerShadow = 1 } })
+			table.insert(ScreenData.MetaUpgradeCardLayout.BonePositions, templateBonePosition) -- Fix crash due to missing "bone position" data
 		end
 	end
 
@@ -54,4 +62,13 @@ ModUtil.LoadOnce(function ()
 	addAdditionalUpgrades(MetaUpgradeCardData.MetaToRunUpgrade, true) -- Skipping because the legendary upgrade doesn't work correctly
 	addAdditionalUpgrades(MetaUpgradeCardData.EpicRarityBoost, false)
 	addAdditionalUpgrades(MetaUpgradeCardData.CardDraw, false)
-end)
+end
+
+-- "Post-Launch Patch 2" added code that reverts the MetaUpgradeState.Level to the max available level
+-- when the code is run, essentially going out of its way to break this mod.
+-- Thus we need to patch the MetaUpgradeState before that code is run.
+-- Doing it in ModUtil.LoadOnce() is too late, so instead we run it before DoPatches()
+ModUtil.Path.Wrap("DoPatches", function(baseFunc)
+	applyExtendedMetaUpgradeData()
+	return baseFunc()
+end, ExtendedAltarOfAshes)
